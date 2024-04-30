62ad9e3753eb546a4097a01a550d2e42a26b73bf
DYLETT <112850430+DYLETT@users.noreply.github.com>
2024-04-30 09:35:41 +0700

    Add files via upload

---
+++ homeWorks/homeWork14/homeWork.py
@@ -0,0 +1,5 @@
+def printer():
+    print('Hello, world')
+
+def printer2():
+    print('Hello, world!')
---
+++ homeWorks/homeWork14/main.py
@@ -0,0 +1,5 @@
+from homeWork import printer
+from homeWork import printer2
+
+printer()
+printer2()
